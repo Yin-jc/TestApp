906d09a8108d90c87665b06c00625420062a5545
Yjc <Yin.jc.0314@gmail.com>
2020-12-10 18:59:23 +0800

    Modify Thread State Test Case

--- appG/src/test/java/com/example/appf/ExampleUnitTest.java
+++ appG/src/test/java/com/example/appf/ExampleUnitTest.java
@@ -24,10 +24,48 @@ public class ExampleUnitTest {
     @Test
     public void testThread() {
         Object lock = new Object();
+        /**
+         * t与t2
+         * wait超时 若拿不到锁 则进入blocked 过程中timed_waiting
+         */
         Thread t = new Thread(() -> {
             synchronized (lock) {
                 try {
                     lock.wait(5000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        Thread t2 = new Thread(() -> {
+            synchronized (lock) {
+                while (true) {
+
+                }
+            }
+        });
+
+        /**
+         * t2与t3
+         * synchronized拿不到锁 直接进入blocked
+         */
+        Thread t3 = new Thread(() -> {
+            synchronized (lock) {
+                while (true) {
+
+                }
+            }
+        });
+
+        /**
+         * t4与t5
+         * wait notify
+         */
+        Thread t4 = new Thread(() -> {
+            synchronized (lock) {
+                try {
+                    lock.wait();
                     while (true) {
 
                     }
@@ -36,56 +74,35 @@ public class ExampleUnitTest {
                 }
             }
         });
-        t.start();
 
-        Thread t2 = new Thread(() -> {
+        Thread t5 = new Thread(() -> {
             synchronized (lock) {
                 try {
                     Thread.sleep(5000);
+                    lock.notifyAll();
+                    //此处虽然唤醒等待线程池 但此线程并不释放锁
+                    while (true) {
+
+                    }
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
-                lock.notifyAll();
             }
         });
+
+
+//        t.start();
 //        t2.start();
-
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-        Thread t3 = new Thread(() -> {
-            synchronized (lock) {
-                System.out.println("t3 get lock");
-                int cnt = 0;
-                while (cnt < 5) {
-                    cnt++;
-                    try {
-                        Thread.sleep(1000);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
-                lock.notifyAll();
-                cnt = 0;
-                while (cnt < 5) {
-                    cnt++;
-                    try {
-                        Thread.sleep(1000);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-        });
-        t3.start();
+//        t3.start();
+        t4.start();
+        t5.start();
 
         while (true) {
-            System.out.println(t.getName() + " state is " + t.getState());
+//            System.out.println(t.getName() + " state is " + t.getState());
 //            System.out.println(t2.getName() + " state is " + t2.getState());
-            System.out.println(t3.getName() + " state is " + t3.getState());
+//            System.out.println(t3.getName() + " state is " + t3.getState());
+            System.out.println(t4.getName() + " state is " + t4.getState());
+            System.out.println(t5.getName() + " state is " + t5.getState());
             try {
                 Thread.sleep(1000);
             } catch (InterruptedException e) {
